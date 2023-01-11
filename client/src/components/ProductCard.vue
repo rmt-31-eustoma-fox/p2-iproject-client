5853806d3f3f32004b16af8b2de8b7c408efa244
Aldrino Wibowo <mraldrino@gmail.com>
2023-01-11 09:13:03 +0700

    home page done

--- client/src/components/ProductCard.vue
+++ client/src/components/ProductCard.vue
@@ -1,2 +1,47 @@
-<script></script>
-<template></template>
+<script>
+import { mapActions, mapWritableState } from "pinia";
+import { useCounterStore } from "@/stores/counter";
+
+export default {
+  props: ["product"],
+  methods: {
+    ...mapActions(useCounterStore, ["addCartHandler"]),
+  },
+};
+</script>
+
+<template>
+  <div v-if="product" class="relative block overflow-hidden group">
+    <RouterLink :to="`/products/${product.id}`">
+      <img
+        :src="product.imgUrl"
+        alt=""
+        class="object-contain w-full h-64 transition duration-500 group-hover:scale-105 sm:h-72"
+      />
+    </RouterLink>
+
+    <div class="relative p-6 bg-white border border-gray-100">
+      <RouterLink :to="`/products/${product.id}`">
+        <h3
+          class="mt-4 text-lg font-medium text-gray-900 truncate hover:whitespace-normal"
+        >
+          {{ product.name }}
+        </h3>
+
+        <p class="mt-1.5 text-base text-gray-700">Rp.{{ product.price }},-</p>
+      </RouterLink>
+
+      <form class="mt-4">
+        <button
+          v-if="product"
+          class="block w-full p-4 text-white font-medium transition bg-[#dda15e] rounded hover:scale-105"
+          @click.prevent="addCartHandler(product.id)"
+        >
+          Add to Cart
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<style></style>
--- client/src/views/HomePage.vue
+++ client/src/views/HomePage.vue
@@ -1,2 +1,63 @@
-<script></script>
-<template></template>
+<script>
+import ProductList from "../components/ProductList.vue";
+import { mapWritableState, mapActions } from "pinia";
+import { useCounterStore } from "../stores/counter";
+
+export default {
+  components: {
+    ProductList,
+  },
+  methods: {
+    ...mapActions(useCounterStore, ["fetchProduct", "fetchCategory"]),
+  },
+
+  computed: {
+    ...mapWritableState(useCounterStore, [
+      "categories",
+      "categorySort",
+      "options",
+    ]),
+  },
+  created() {
+    this.fetchProduct(this.options);
+    this.fetchCategory();
+  },
+  watch: {
+    categorySort(newValue, oldValue) {
+      this.options.CategoryId = newValue;
+      this.fetchProduct(this.options);
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="container grid grid-cols-5 gap-6 pt-10 pb-16 items-start mx-auto">
+    <!-- products -->
+    <div class="col-span-3 col-start-2">
+      <div class="flex items-center mb-4">
+        <select
+          v-model="categorySort"
+          v-on:change="fetchProduct"
+          name="sort"
+          id="sort"
+          class="w-44 text-sm text-gray-600 py-3 px-4 border-gray-300 shadow-sm rounded focus:ring-primary focus:border-primary"
+        >
+          <option value="">All Category</option>
+          <option
+            v-for="category in categories"
+            :key="category.id"
+            :value="category.id"
+          >
+            {{ category.name }}
+          </option>
+        </select>
+      </div>
+
+      <ProductList />
+    </div>
+    <!-- ./products -->
+  </div>
+</template>
+
+<style></style>
